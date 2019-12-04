b36ff5d1dd50f09d2dd8b01f879d3a2b05103ced
work-ilya-web <ilya.balyasov48@gmail.com>
2019-12-05 00:03:51 +0300

    animate

--- js/common.js
+++ js/common.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
 
     sliderRun(); // запускаем промо слайдер
 
-	// Address visible
+    // Address visible
     if ($(".header-address__caption").length > 0) {
-        $(".header-address__caption").click(function(){
-        	$("body").toggleClass("lock");
+        $(".header-address__caption").click(function () {
+            $("body").toggleClass("lock");
             $(this).toggleClass("active");
             $(".header-address__desc").toggleClass("active");
         });
     }
 
-	// Main menu open
+    // Main menu open
     if ($(".header__burger").length > 0) {
-        $(".header__burger").click(function(){
+        $(".header__burger").click(function () {
             $(this).toggleClass("active");
             $(".menu").toggleClass("active");
             $(".menu__mobile").toggleClass("active");
@@ -22,119 +22,121 @@ $(document).ready(function () {
 
     // Promo slider
     if ($(".promo__slick").length > 0) {
-	    $('.promo__slick').slick({
-	        dots: false,
-	        arrows: false,
-	        speed: 500,
-	        slidesToShow: 1,
-	        slidesToScroll: 1,
-	        fade: true,
-	        responsive: [
+        $('.promo__slick').slick({
+            dots: false,
+            arrows: false,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            responsive: [
                 {
                     breakpoint: 760,
                     settings: {
                         slidesToShow: 1,
-	        			slidesToScroll: 1,
-	        			adaptiveHeight: true
+                        slidesToScroll: 1,
+                        adaptiveHeight: true
                     }
                 }
             ]
-	    });
-	    $('.promo-arrow').click(function (event) {
-	        $(this).parents('.promo').find('.promo__slick').slick('slickNext');
+        });
+        $('.promo-arrow').click(function (event) {
+            $(this).parents('.promo').find('.promo__slick').slick('slickNext');
             $(".progress-slider").html('<span></span>');
             $('.promo__slick--js').attr('data-step', 1);
 
-	    });
-	    $('.promo__slick').on('afterChange', function(event, slick, currentSlide){
-			productsliderinfo();
-		});
-		function productsliderinfo(){
-			$('.promo__info-box').html("0" + $('.promo__slide').length);
-		}
-		productsliderinfo();
-		$(".promo__slick").on('afterChange', function(event, slick, currentSlide){
-			var promonumber = (currentSlide + 1);
-	    	$(".promo__info-number").html("0" + promonumber);
-	  	});
-	}
+        });
+        $('.promo__slick').on('afterChange', function (event, slick, currentSlide) {
+            productsliderinfo();
+        });
+
+        function productsliderinfo() {
+            $('.promo__info-box').html("0" + $('.promo__slide').length);
+        }
+        productsliderinfo();
+        $(".promo__slick").on('afterChange', function (event, slick, currentSlide) {
+            var promonumber = (currentSlide + 1);
+            $(".promo__info-number").html("0" + promonumber);
+        });
+    }
 
     // Promo slider
     if ($(".equipment__slick").length > 0) {
-	    $('.equipment__slick').slick({
-	        dots: false,
-	        arrows: false,
-	        speed: 500,
-	        slidesToShow: 1,
-	        slidesToScroll: 1,
-	        fade: true,
-	        responsive: [
+        $('.equipment__slick').slick({
+            dots: false,
+            arrows: false,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            responsive: [
                 {
                     breakpoint: 760,
                     settings: {
                         slidesToShow: 1,
-	        			slidesToScroll: 1,
-	        			adaptiveHeight: true
+                        slidesToScroll: 1,
+                        adaptiveHeight: true
                     }
                 }
             ]
-	    });
-	    $('.equipment-arrow').click(function (event) {
-	        $(this).parents('.equipment').find('.equipment__slick').slick('slickNext');
-	    });
-	    $('.equipment__slick').on('afterChange', function(event, slick, currentSlide){
-			productsliderinfo();
-		});
-		function productsliderinfo(){
-			$('.equipment__info-box').html("0" + $('.equipment__slide').length);
-		}
-		productsliderinfo();
-		$(".equipment__slick").on('afterChange', function(event, slick, currentSlide){
-			var promonumber = (currentSlide + 1);
-	    	$(".equipment__info-number").html("0" + promonumber);
-	  	});
-	}
+        });
+        $('.equipment-arrow').click(function (event) {
+            $(this).parents('.equipment').find('.equipment__slick').slick('slickNext');
+        });
+        $('.equipment__slick').on('afterChange', function (event, slick, currentSlide) {
+            productsliderinfo();
+        });
+
+        function productsliderinfo() {
+            $('.equipment__info-box').html("0" + $('.equipment__slide').length);
+        }
+        productsliderinfo();
+        $(".equipment__slick").on('afterChange', function (event, slick, currentSlide) {
+            var promonumber = (currentSlide + 1);
+            $(".equipment__info-number").html("0" + promonumber);
+        });
+    }
 
     var swiperHome = $('.doctors__wrap');
-    swiperHome.each(function(){
+    swiperHome.each(function () {
         var mySwiper = new Swiper(this, {
             slidesPerView: 3.6,
-			spaceBetween: 10,
-			slidesPerGroup: 1,
-			loop: false,
-			loopFillGroupWithBlank: false,
+            spaceBetween: 10,
+            slidesPerGroup: 1,
+            loop: false,
+            loopFillGroupWithBlank: false,
             speed: 1000,
             iOSEdgeSwipeDetection: true,
-			scrollbar: {
-				el: '.swiper-scrollbar',
-				draggable: true,
-				hide: false,
-				dragSize: 20,
-				snapOnRelease: false,
-			},
-		    breakpoints: {
-		    	370: {
-	            	slidesPerView: 1,
-					slidesPerGroup: 1,
-	            },
-		    	760: {
-	            	slidesPerView: 1.6,
-					slidesPerGroup: 1,
-	            },
-	            1010: {
-	            	slidesPerView: 2.6,
-					slidesPerGroup: 1,
-	            }
-	        }
+            scrollbar: {
+                el: '.swiper-scrollbar',
+                draggable: true,
+                hide: false,
+                dragSize: 20,
+                snapOnRelease: false,
+            },
+            breakpoints: {
+                370: {
+                    slidesPerView: 1,
+                    slidesPerGroup: 1,
+                },
+                760: {
+                    slidesPerView: 1.6,
+                    slidesPerGroup: 1,
+                },
+                1010: {
+                    slidesPerView: 2.6,
+                    slidesPerGroup: 1,
+                }
+            }
         });
     });
 
     // Product tabs
     if ($(".services__tab").length > 0) {
-        $(function() {
-            $('.services__tabs').each(function() {
-                $(this).find('.services__tab').each(function(i) {
-                    $(this).click(function(){
+        $(function () {
+            $('.services__tabs').each(function () {
+                $(this).find('.services__tab').each(function (i) {
+                    $(this).click(function () {
                         $(this).addClass('active').siblings().removeClass('active').closest('.services__wrap').find('.services-top__block').removeClass('active').eq(i).addClass('active').closest('.services__wrap').find('.services__block').removeClass('active').eq(i).addClass('active');
                     });
                 });
@@ -144,7 +146,7 @@ $(document).ready(function () {
 
     // calendar mask
     $('.datepicker-here').datepicker({
-        onSelect: function(date, cellType, inst) {
+        onSelect: function (date, cellType, inst) {
             $(inst.el).parent().removeClass('input-field__error').addClass('input-field__ok');
             var myDatepicker = $(inst.el).datepicker().data('datepicker');
             myDatepicker.hide();
@@ -162,6 +164,7 @@ $(document).ready(function () {
     $(".phone-mask").mask("+7 (999)999-99-99");
 
     google.maps.event.addDomListener(window, 'load', init);
+
     function init() {
         var mapOptions = {
             zoom: 16,
@@ -179,42 +182,54 @@ $(document).ready(function () {
         });
 
         var info = new google.maps.InfoWindow({
-        	content: '<small>адрес клиники</small><p>Подольск, ул. Академика<br> Доллежаля д.4</p>'
+            content: '<small>адрес клиники</small><p>Подольск, ул. Академика<br> Доллежаля д.4</p>'
         });
 
-        marker.addListener("click", function() {
-        	info.open(map, marker);
+        marker.addListener("click", function () {
+            info.open(map, marker);
         });
     }
 
-    if ($(window).outerWidth() >= '1260'){
-		const containerID = '#confidentiality-modal';
-		const scrollbar = Scrollbar.init(document.querySelector(containerID), {
-			damping: 0.1,
-			alwaysShowTracks: true
-		});
-	}
+    if ($(window).outerWidth() >= '1260') {
+        const containerID = '#confidentiality-modal';
+        const scrollbar = Scrollbar.init(document.querySelector(containerID), {
+            damping: 0.1,
+            alwaysShowTracks: true
+        });
+    }
 
-	// Fancybox
-	$(".footer__confidentiality").fancybox({
-		"hideOnContentClick" :false,
-	});
+    // Fancybox
+    $(".footer__confidentiality").fancybox({
+        "hideOnContentClick": false,
+    });
 
 
+
+    inView('.main-doctor__after').on('enter', function (event, isInView) {
+        $('.main-doctor__after').addClass('active');
+    }).on('exit', function (event, isInView) {
+        $('.main-doctor__after').removeClass('active');
+    });
+
+    inView('.subscription__img img').on('enter', function (event, isInView) {
+        $('.subscription__img img').addClass('active');
+    }).on('exit', function (event, isInView) {
+        $('.subscription__img img').removeClass('active');
+    });
 
 });
 
 // timer slider
 function sliderRun() {
 
-     setInterval(function () {
+    setInterval(function () {
         var step = parseInt($('.promo__slick--js').attr('data-step'));
         var widthPromoBtn = $(".progress-slider>span").width();
         var progressSliderWidth = $(".progress-slider").width();
 
         if (step < 101) {
-            $(".progress-slider>span").css('width',''+step+'%');
-            $('.promo-arrow__icon').attr('class',' promo-arrow__icon c100 p'+step);
+            $(".progress-slider>span").css('width', '' + step + '%');
+            $('.promo-arrow__icon').attr('class', ' promo-arrow__icon c100 p' + step);
             $('.promo__slick--js').attr('data-step', step + 1);
         } else {
             $('.promo__slick--js').attr('data-step', 1);
@@ -224,3 +239,4 @@ function sliderRun() {
 
     }, 100);
 }
+
